4b94c41c6f1eb1a5fa0e2c111b0bfbf00d84f073
RagexPrince683 <yellowtoad91@gmail.com>
2024-08-03 02:53:17 -0400

    not working thing

--- src/main/java/com/Ragex/mcheliloader/mcheliloader.java
+++ src/main/java/com/Ragex/mcheliloader/mcheliloader.java
@@ -3,6 +3,7 @@ package com.Ragex.mcheliloader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.*;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,6 +14,11 @@ import java.util.zip.ZipEntry;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.apache.commons.io.FileUtils.copyDirectory;
+
 
 //import static cpw.mods.fml.common.Loader.minecraftDir;
 //import net.minecraftforge.common.
@@ -27,20 +33,21 @@ import java.nio.file.*;
 
 
 public class mcheliloader {
-    Path AssetsourceDir = Paths.get("assets/mcheli");
+    private static final Path AssetsourceDir = Paths.get("assets/mcheli");
 
-    Path CodesourceDir = Paths.get("code/mcheli");
+    private static final Path CodesourceDir = Paths.get("code/mcheli");
 
 
     @Mod.EventHandler
     public void PreInit(FMLPreInitializationEvent evt) {
-        //Logger logger = LogManager.getLogger(mcheliloader.class.getName());
+        Logger logger = LogManager.getLogger(mcheliloader.class.getName());
         try {
             File configDir = evt.getModConfigurationDirectory();
             File minecraftDir = configDir.getParentFile();
             Path modsDir = Paths.get(minecraftDir.getPath(), "mods");
             Path mcheliDir = Paths.get(modsDir.toString(), "mcheli");
             Path mcheliAssetsDir = Paths.get(modsDir.toString(), "mcheli/assets");
+            Path mcheliCodeDir = Paths.get(mcheliDir.toString(), "mcheli");
             //Path mcheliCodeDir = Paths.get(modsDir.toString(), "mcheli");
             if (!Files.exists(mcheliDir)) {
                 //assets need to go into mods/mcheli/assets/mcheli
@@ -51,42 +58,79 @@ public class mcheliloader {
                 }
 
             }
+            FileUtils.copyDirectory(CodesourceDir.toFile(), mcheliCodeDir.toFile());
+            FileUtils.copyDirectory(AssetsourceDir.toFile(), mcheliAssetsDir.toFile());
 
-            System.out.println("Copying from " + CodesourceDir + " to " + mcheliDir);
 
-            Files.walk(CodesourceDir).forEach(source -> {
-                Path destination = Paths.get(mcheliDir.toString(), source.toString().substring(CodesourceDir.toString().length()));
-                try {
-                    Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e) {
-                    e.printStackTrace();
+            AtomicBoolean isCodeCopied = new AtomicBoolean(true);
+            AtomicBoolean isAssetsCopied = new AtomicBoolean(true);
+
+            SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Path targetFile = mcheliDir.resolve(file.subpath(0, file.getNameCount()).relativize(file));
+                    Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+                    return FileVisitResult.CONTINUE;
                 }
-            });
 
-            System.out.println("Copying from " + AssetsourceDir + " to " + mcheliAssetsDir);
-
-
-            Files.walk(AssetsourceDir).forEach(source -> {
-                Path destination = Paths.get(mcheliAssetsDir.toString(), source.toString().substring(AssetsourceDir.toString().length()));
-                try {
-                    Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    if (exc!= null) {
+                        throw exc;
+                    }
+                    return FileVisitResult.CONTINUE;
                 }
-            });
+            };
 
-            boolean isCodeCopied = Files.walk(mcheliDir).count() == Files.walk(CodesourceDir).count();
-            boolean isAssetsCopied = Files.walk(mcheliAssetsDir).count() == Files.walk(AssetsourceDir).count();
+            Files.walkFileTree(CodesourceDir, visitor);
+            Files.walkFileTree(AssetsourceDir, visitor);
 
-            if (!isCodeCopied || !isAssetsCopied) {
-                // Handle the error
-                System.out.println("Error: Files were not copied correctly.");
+            // Validate if all files were copied successfully
+            long originalCodeFiles = Files.walk(CodesourceDir).count();
+            long copiedCodeFiles = Files.walk(mcheliDir).count();
+            long originalAssetsFiles = Files.walk(AssetsourceDir).count();
+            long copiedAssetsFiles = Files.walk(mcheliAssetsDir).count();
+
+            if (originalCodeFiles!= copiedCodeFiles || originalAssetsFiles!= copiedAssetsFiles) {
+                logger.error("Error: Not all files were copied correctly.");
             }
+
+        //    System.out.println("Copying from " + CodesourceDir + " to " + mcheliDir);
+
+      //      Files.walk(CodesourceDir).forEach(source -> {
+      //          Path destination = Paths.get(mcheliDir.toString(), source.toString().substring(CodesourceDir.toString().length()));
+      //          try {
+      //              Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+      //          } catch (IOException e) {
+      //              e.printStackTrace();
+      //          }
+      //      });
+//
+      //      System.out.println("Copying from " + AssetsourceDir + " to " + mcheliAssetsDir);
+
+
+     //       Files.walk(AssetsourceDir).forEach(source -> {
+     //           Path destination = Paths.get(mcheliAssetsDir.toString(), source.toString().substring(AssetsourceDir.toString().length()));
+     //           try {
+     //               Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+     //           } catch (IOException e) {
+     //               e.printStackTrace();
+     //           }
+     //       });
+
+            //boolean isCodeCopied = Files.walk(mcheliDir).count() == Files.walk(CodesourceDir).count();
+           // boolean isAssetsCopied = Files.walk(mcheliAssetsDir).count() == Files.walk(AssetsourceDir).count();
+
+     //       if (!isCodeCopied || !isAssetsCopied) {
+     //           // Handle the error
+     //           System.out.println("Error: Files were not copied correctly.");
+     //       }
 
 
 
         } catch (Exception e) {
             e.printStackTrace();
+            logger.error("An error occurred during pre-initialization.", e);
         }
     }
 }
