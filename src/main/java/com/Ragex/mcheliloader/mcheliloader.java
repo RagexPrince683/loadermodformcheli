73ec90ade459394f425eff7cdcf9418875f6adf0
RagexPrince683 <yellowtoad91@gmail.com>
2024-08-20 23:02:55 -0400

    idk anymore

--- src/main/java/com/Ragex/mcheliloader/mcheliloader.java
+++ src/main/java/com/Ragex/mcheliloader/mcheliloader.java
@@ -1,9 +1,18 @@
 package com.Ragex.mcheliloader;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Paths;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,114 +24,61 @@ import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import static com.hbm.main.MainRegistry.configDir;
+
+
 @Mod(
         modid = "mcheliloader",
         name = "mcheliloader",
         dependencies = "required-after:Forge@[10.13.2.1230,)"
 )
 public class mcheliloader {
+    File minecraftDir = configDir.getParentFile();
     private static final Logger LOGGER = LogManager.getLogger(mcheliloader.class.getName());
-    private static final int MAX_RETRIES = 30;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        try {
-            File configDir = event.getModConfigurationDirectory();
-            File minecraftDir = configDir.getParentFile();
-            Path modsDir = Paths.get(minecraftDir.getPath(), "mods");
-            Path mcheliDir = modsDir.resolve("mcheli");
-            Path mcheliAssetsDir = mcheliDir.resolve("assets/mcheli");
-            Path mcheliCodeDir = mcheliDir.resolve("mcheli");
+        String[] fileURLs = {
+                "https://supporter-files.nexus-cdn.com/197/375/Mcheli-O%20V1.7.1-375-1-7-10-1722557745.zip?md5=wyCG2ecseY2uroQwW7voxA&expires=1724092780&user_id=178368000"
+        };
 
-            createDirectories(mcheliDir, mcheliAssetsDir, mcheliCodeDir);
+        Path modsDir = Paths.get(minecraftDir.getPath(), "mods");
 
-            copyDirectoryFromJar("assets/mcheli", mcheliAssetsDir);
-            copyDirectoryFromJar("code/mcheli", mcheliCodeDir);
+        String DownloadDir = modsDir.toString();
 
-            validateFiles("assets/mcheli", mcheliAssetsDir);
-            validateFiles("code/mcheli", mcheliCodeDir);
-
-        } catch (Exception e) {
-            LOGGER.error("An error occurred during pre-initialization.", e);
-        }
-    }
-
-    private void createDirectories(Path... paths) throws IOException {
-        for (Path path : paths) {
-            if (!Files.exists(path)) {
-                Files.createDirectories(path);
-                LOGGER.debug("Created directory: " + path);
-            }
-        }
-    }
-
-    private void copyDirectoryFromJar(String sourceDir, Path targetDir) throws IOException, URISyntaxException {
-        // Remove leading slash if present
-        if (sourceDir.startsWith("/")) {
-            sourceDir = sourceDir.substring(1);
-        }
-        try (ZipFile jarFile = new ZipFile(new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI()))) {
-            Enumeration<? extends ZipEntry> entries = jarFile.entries();
-            while (entries.hasMoreElements()) {
-                ZipEntry entry = entries.nextElement();
-                if (entry.getName().startsWith(sourceDir)) {
-                    String relativePath = entry.getName().substring(sourceDir.length());
-                    Path targetPath = targetDir.resolve(relativePath).normalize();
-
-                    if (entry.isDirectory()) {
-                        Files.createDirectories(targetPath);
-                    } else {
-                        copyFileWithRetries(jarFile, entry, targetPath);
-                    }
-                    LOGGER.debug("Processed entry: " + entry.getName() + " to " + targetPath);
-                }
-            }
-        } catch (Exception e) {
-            LOGGER.error("Failed to copy directory from jar: " + sourceDir, e);
-            throw new IOException("Failed to copy directory from jar: " + sourceDir, e);
-        }
-    }
-
-    private void copyFileWithRetries(ZipFile jarFile, ZipEntry entry, Path targetPath) throws IOException {
-        int attempt = 0;
-        while (attempt < MAX_RETRIES) {
-            try (InputStream is = jarFile.getInputStream(entry)) {
-                Files.createDirectories(targetPath.getParent());
-                Files.copy(is, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                LOGGER.debug("Copied file: " + entry.getName() + " to " + targetPath);
-                return;
+        for (String fileURL : fileURLs) {
+            try {
+                downloadFile(fileURL, DownloadDir);
+                System.out.println("Downloaded: " + fileURL + " to " + DownloadDir);
             } catch (IOException e) {
-                attempt++;
-                LOGGER.warn("Retry " + attempt + " for copying file: " + entry.getName() + " to " + targetPath, e);
-                if (attempt >= MAX_RETRIES) {
-                    LOGGER.error("Failed to copy file after " + MAX_RETRIES + " attempts: " + entry.getName() + " to " + targetPath, e);
-                    throw e;
-                }
+                System.err.println("Failed to download: " + fileURL);
+                e.printStackTrace();
             }
         }
+
+
+
     }
 
-    private void validateFiles(String sourceDir, Path targetDir) throws IOException, URISyntaxException {
-        long originalFiles = 0;
-        // Remove leading slash if present
-        if (sourceDir.startsWith("/")) {
-            sourceDir = sourceDir.substring(1);
-        }
-        try (ZipFile jarFile = new ZipFile(new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI()))) {
-            Enumeration<? extends ZipEntry> entries = jarFile.entries();
-            while (entries.hasMoreElements()) {
-                ZipEntry entry = entries.nextElement();
-                if (entry.getName().startsWith(sourceDir) && !entry.isDirectory()) {
-                    originalFiles++;
-                }
-            }
-        }
-        long copiedFiles = Files.walk(targetDir).filter(Files::isRegularFile).count();
+    public static void downloadFile(String urlStr, String saveDir) throws IOException {
+        URL url = new URL(urlStr);
+        URLConnection connection = url.openConnection();
+        InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+        String fileName = urlStr.substring(urlStr.lastIndexOf("/") + 1);
+        FileOutputStream outputStream = new FileOutputStream(Paths.get(saveDir, fileName).toString());
 
-        if (originalFiles != copiedFiles) {
-            LOGGER.error("Error: Not all files were copied correctly. Source: " + originalFiles + " files, Target: " + copiedFiles + " files.");
-        } else {
-            LOGGER.info("All files copied successfully from " + sourceDir + " to " + targetDir);
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
         }
+
+        outputStream.close();
+        inputStream.close();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent evt) {
+
     }
 }
