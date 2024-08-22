1655b8999b4388961cfe37a9643a4298e94b0cff
RagexPrince683 <yellowtoad91@gmail.com>
2024-08-22 00:33:05 -0400

    xd

--- src/main/java/com/Ragex/mcheliloader/mcheliloader.java
+++ src/main/java/com/Ragex/mcheliloader/mcheliloader.java
@@ -1,25 +1,17 @@
 package com.Ragex.mcheliloader;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.StandardCopyOption;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -38,46 +30,53 @@ public class mcheliloader {
 
         minecraftDir = event.getModConfigurationDirectory().getParentFile();
         String[] fileURLs = {
-                "https://github.com/RagexPrince683/MCH-mocmaster"
+                "https://github.com/RagexPrince683/mchelio/archive/refs/heads/new-vehicles.zip" // Correct URL for zip file
         };
 
         Path modsDir = Paths.get(minecraftDir.getPath(), "mods");
 
-        String DownloadDir = modsDir.toString();
+        String downloadDir = modsDir.toString();
 
         for (String fileURL : fileURLs) {
             try {
-                downloadFile(fileURL, DownloadDir);
-                System.out.println("Downloaded: " + fileURL + " to " + DownloadDir);
+                Path zipFilePath = downloadFile(fileURL, downloadDir);
+                unzipFile(zipFilePath.toString(), downloadDir);
+                Files.delete(zipFilePath); // Clean up the zip file after extraction
+                System.out.println("Downloaded and extracted: " + fileURL + " to " + downloadDir);
             } catch (IOException e) {
-                System.err.println("Failed to download: " + fileURL);
+                System.err.println("Failed to download or extract: " + fileURL);
                 e.printStackTrace();
             }
         }
-
-
-
     }
 
-    public static void downloadFile(String urlStr, String saveDir) throws IOException {
+    public static Path downloadFile(String urlStr, String saveDir) throws IOException {
         URL url = new URL(urlStr);
         URLConnection connection = url.openConnection();
         InputStream inputStream = new BufferedInputStream(connection.getInputStream());
         String fileName = urlStr.substring(urlStr.lastIndexOf("/") + 1);
-        FileOutputStream outputStream = new FileOutputStream(Paths.get(saveDir, fileName).toString());
+        Path filePath = Paths.get(saveDir, fileName);
+        Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
 
-        byte[] buffer = new byte[1024];
-        int bytesRead;
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            outputStream.write(buffer, 0, bytesRead);
-        }
-
-        outputStream.close();
         inputStream.close();
+        return filePath;
     }
 
-   // @Mod.EventHandler
-   // public void init(FMLInitializationEvent evt) {
-//
-   // }
+    public static void unzipFile(String zipFilePath, String destDir) throws IOException {
+        try (ZipFile zipFile = new ZipFile(zipFilePath)) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                Path filePath = Paths.get(destDir, entry.getName());
+
+                if (entry.isDirectory()) {
+                    Files.createDirectories(filePath);
+                } else {
+                    Files.createDirectories(filePath.getParent());
+                    Files.copy(zipFile.getInputStream(entry), filePath, StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        }
+    }
 }
